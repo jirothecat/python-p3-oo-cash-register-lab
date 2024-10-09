7ef27175bf93cd2c4df89d77fb9a86ee08344c14
Dylan Tin <dylant711@gmail.com>
2024-10-09 14:55:36 -0400

    lab review cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  
+    def __init__(self, discount=0):
+        self.discount = discount 
+        self.total = 0
+        self.items = []
+      
+    def add_item(self, title, price, quantity=1):
+        self.total = self.total + (price * quantity)
+        for index in range(quantity):
+          self.items.append(title)
+
+        self.last_transaction = price * quantity
+
+    def apply_discount(self):
+        self.total = self.total - (self.total * (self.discount / 100))
+        if not (self.discount == 0):
+          print(f"After the discount, the total comes to ${int(self.total)}.")  
+        else:
+          print("There is no discount to apply.")     
+
+    def void_last_transaction(self):
+       self.total = self.total - self.last_transaction
